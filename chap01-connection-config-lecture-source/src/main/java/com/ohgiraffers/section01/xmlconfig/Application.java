d84f00e7b826a46e86cb08936fb8e892e17006aa
indy032229@gmail.com <indy032229@gmail.com>
2025-09-11 11:05:10 +0900

    2025/09/11

---
+++ chap01-connection-config-lecture-source/src/main/java/com/ohgiraffers/section01/xmlconfig/Application.java
@@ -0,0 +1,38 @@
+package com.ohgiraffers.section01.xmlconfig;
+
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+public class Application {
+    public static void main(String[] args) {
+        String resource = "mybatis-config.xml";
+
+        /* 설명. JDBC에서의 Connection 객체 같은 개념 */
+        SqlSession session = null;
+
+        try {
+            InputStream inputStream = Resources.getResourceAsStream(resource);
+
+            SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(inputStream);
+
+            session = sqlSessionFactory.openSession(false); // false를 주어야 수동 커밋 가능
+
+            java.util.Date newDate = session.selectOne("mapper.selectNow");
+
+            System.out.println("newDate = " + newDate);
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }finally {
+            if (session != null) {
+                session.close();
+            }
+
+        }
+    }
+}
